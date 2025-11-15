34fb35bb49ffc4285e9bd05a3b9f4003eceabc34
alamRodriguez <alamrd2016@gmail.com>
2025-11-14 23:41:45 -0400

    enviroemts GOOGLE_REDIRECT_URI added

--- api/auth/google.js
+++ api/auth/google.js
@@ -27,7 +27,7 @@ export const googleCallback = async (req, res) => {
             code,
             client_id: process.env.GOOGLE_CLIENT_ID,
             client_secret: process.env.GOOGLE_CLIENT_SECRET,
-            redirect_uri: "http://localhost:3001/api/auth/google/callback",
+            redirect_uri: process.env.GOOGLE_REDIRECT_URI,
             grant_type: "authorization_code",
         });
 
